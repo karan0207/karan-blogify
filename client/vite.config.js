4bc937471289c08c2e400056acdea8da71a396ca
Karan0207 <minidox0207@gmail.com>
2024-05-03 00:26:12 +0530

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.BASE_URL||"https://karan-blogify.onrender.com",
         secure: false,
       },
     },
